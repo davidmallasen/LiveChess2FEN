3a921af06b163c5acc9337d95d88bc6bb6cd8579
David Mallasen <dmallasen@gmail.com>
2022-05-04 12:38:54 +0200

    Update keras2onnx

--- cpmodels/kerasmodel2onxx.py
+++ cpmodels/kerasmodel2onxx.py
@@ -1,60 +1,30 @@
 """
 Convert Keras model to ONNX format.
-
-After converting the model to onnx format, to create the TensorRT
-engine using onnx2trt execute the following command:
-
-onnx2trt MobileNetV2_2.onnx -o MobileNetV2_2_engine.trt -b 1
-  -w 268435456 -d 16 -lgv
 """
-import keras2onnx
+import tensorflow as tf
+import tf2onnx
 
-from keras.applications import NASNetMobile, MobileNetV2
-from keras.models import load_model, clone_model
-
-from chess_piece_models_common import build_model
-from squeezenet import SqueezeNet
-from train_alexnet import alexnet
-
-KERAS_MODEL_PATH = "../selected_models/NASNetMobile_all_last.h5"
-ONNX_MODEL_PATH = KERAS_MODEL_PATH[:-2] + "onnx"
+MODELS_PATH = "../selected_models/"
+KERAS_MODEL_NAME = "MobileNetV2_0p5_all.h5"
+ONNX_MODEL_NAME = KERAS_MODEL_NAME[:-2] + "onnx"
 
 image_size = 224
 channels = 3
 batch_size = 1
-target_opset = 8
+target_opset = 13
 
 
 def main():
-    """Converts a keras model into ONNX format."""
-    # model = alexnet((224, 224, 3))
-    model = build_model(NASNetMobile(input_shape=(224, 224, 3),
-                                     include_top=False,
-                                     weights='imagenet'))
-    model.load_weights(KERAS_MODEL_PATH)
+    # Convert the .h5 keras model to .pb
+    keras_model = tf.keras.models.load_model(MODELS_PATH + KERAS_MODEL_NAME)
+    keras_model.save(MODELS_PATH + "saved_model")
 
-    # If we have not specified explicitly image dimensions when creating
-    # the model
-    #
-    # model = load_model(KERAS_MODEL_PATH)
-    # model._layers[0].batch_input_shape = (batch_size, image_size, image_size,
-    #                                       channels)
-    #
-    # In order for the input_shape to be saved correctly we have to
-    # clone the model into a new one
-    #
-    # model = clone_model(model)
-    #
-    # When cloning we loose the weights, load them again
-    #
-    # model.load_weights(KERAS_MODEL_PATH)
-
-    onnx_model = keras2onnx.convert_keras(model, model.name)
-
-    # target_opset=target_opset,
-    # debug_mode=True
-
-    keras2onnx.save_model(onnx_model, ONNX_MODEL_PATH)
+    # Convert to ONNX
+    spec = (tf.TensorSpec((batch_size, image_size, image_size, channels), tf.float32, name="input"),)
+    model_proto, _ = tf2onnx.convert.from_keras(keras_model, 
+                                                input_signature=spec, 
+                                                opset=target_opset, 
+                                                output_path=MODELS_PATH + ONNX_MODEL_NAME)
 
 
 if __name__ == "__main__":
