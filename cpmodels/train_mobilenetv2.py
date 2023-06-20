7353475787b693d71a101902370d78585bb37e1f
David-davidlxl <davidlxl@umich.edu>
2023-06-20 18:11:32 -0400

    Update code for PEP-8 style and compatibility with tensorflow 2.11.0

--- cpmodels/train_mobilenetv2.py
+++ cpmodels/train_mobilenetv2.py
@@ -3,16 +3,23 @@ Train MobileNetV2 model.
 """
 from keras.applications import MobileNetV2
 from keras.applications.mobilenet_v2 import preprocess_input
-from keras.engine.saving import load_model
+from keras.models import load_model
 
-from chess_piece_models_common import build_model, data_generators, \
-    train_model, plot_model_history, evaluate_model, model_callbacks
+from chess_piece_models_common import (
+    build_model,
+    data_generators,
+    train_model,
+    plot_model_history,
+    evaluate_model,
+    model_callbacks,
+)
 
 
 def train_chesspiece_model():
     """Trains the chesspiece model based on MobileNetV2."""
-    base_model = MobileNetV2(input_shape=(224, 224, 3), include_top=False,
-                             weights='imagenet', alpha=0.5)
+    base_model = MobileNetV2(
+        input_shape=(224, 224, 3), include_top=False, weights="imagenet", alpha=0.5
+    )
 
     # First train only the top layers
     for layer in base_model.layers:
@@ -21,15 +28,26 @@ def train_chesspiece_model():
     model = build_model(base_model)
 
     train_generator, validation_generator = data_generators(
-        preprocess_input, (224, 224), 64)
+        preprocess_input, (224, 224), 64
+    )
 
     callbacks = model_callbacks(5, "./models/MobileNetV2_0p5_pre.h5", 0.1, 10)
 
-    history = train_model(model, 20, train_generator, validation_generator,
-                          callbacks, use_weights=False, workers=10)
+    history = train_model(
+        model,
+        20,
+        train_generator,
+        validation_generator,
+        callbacks,
+        use_weights=False,
+        workers=10,
+    )
 
-    plot_model_history(history, "./models/MobileNetV2_0p5_pre_acc.png",
-                       "./models/MobileNetV2_0p5_pre_loss.png")
+    plot_model_history(
+        history,
+        "./models/MobileNetV2_0p5_pre_acc.png",
+        "./models/MobileNetV2_0p5_pre_loss.png",
+    )
     evaluate_model(model, validation_generator)
 
     # Also train blocks 14-16
@@ -38,16 +56,25 @@ def train_chesspiece_model():
     for layer in model.layers[126:]:
         layer.trainable = True
 
-    model.compile(optimizer='Adam', loss='categorical_crossentropy',
-                  metrics=['accuracy'])
+    model.compile(
+        optimizer="Adam", loss="categorical_crossentropy", metrics=["accuracy"]
+    )
 
     callbacks = model_callbacks(20, "./models/MobileNetV2_0p5.h5", 0.2, 8)
 
-    history = train_model(model, 100, train_generator, validation_generator,
-                          callbacks, use_weights=False, workers=10)
+    history = train_model(
+        model,
+        100,
+        train_generator,
+        validation_generator,
+        callbacks,
+        use_weights=False,
+        workers=10,
+    )
 
-    plot_model_history(history, "./models/MobileNetV2_0p5_acc.png",
-                       "./models/MobileNetV2_0p5_loss.png")
+    plot_model_history(
+        history, "./models/MobileNetV2_0p5_acc.png", "./models/MobileNetV2_0p5_loss.png"
+    )
     evaluate_model(model, validation_generator)
 
     model.save("./models/MobileNetV2_0p5_last.h5")
@@ -58,22 +85,34 @@ def continue_training():
     model = load_model("./models/MobileNetV2_0p5.h5")
 
     train_generator, validation_generator = data_generators(
-        preprocess_input, (224, 224), 64)
+        preprocess_input, (224, 224), 64
+    )
 
     # Train all layers
     for layer in model.layers:
         layer.trainable = True
 
-    model.compile(optimizer='Adam', loss='categorical_crossentropy',
-                  metrics=['accuracy'])
+    model.compile(
+        optimizer="Adam", loss="categorical_crossentropy", metrics=["accuracy"]
+    )
 
     callbacks = model_callbacks(20, "./models/MobileNetV2_0p5_all.h5", 0.2, 8)
 
-    history = train_model(model, 100, train_generator, validation_generator,
-                          callbacks, use_weights=False, workers=10)
+    history = train_model(
+        model,
+        100,
+        train_generator,
+        validation_generator,
+        callbacks,
+        use_weights=False,
+        workers=10,
+    )
 
-    plot_model_history(history, "./models/MobileNetV2_0p5_all_acc.png",
-                       "./models/MobileNetV2_0p5_all_loss.png")
+    plot_model_history(
+        history,
+        "./models/MobileNetV2_0p5_all_acc.png",
+        "./models/MobileNetV2_0p5_all_loss.png",
+    )
     evaluate_model(model, validation_generator)
 
     model.save("./models/MobileNetV2_0p5_all_last.h5")
--- cpmodels/train_xception.py
+++ cpmodels/train_xception.py
@@ -3,16 +3,23 @@ Train Xception model.
 """
 from keras.applications import Xception
 from keras.applications.xception import preprocess_input
-from keras.engine.saving import load_model
+from keras.models import load_model
 
-from chess_piece_models_common import build_model, data_generators, \
-    train_model, plot_model_history, evaluate_model, model_callbacks
+from chess_piece_models_common import (
+    build_model,
+    data_generators,
+    train_model,
+    plot_model_history,
+    evaluate_model,
+    model_callbacks,
+)
 
 
 def train_chesspiece_model():
     """Trains the chesspiece model based on Xception."""
-    base_model = Xception(input_shape=(299, 299, 3), include_top=False,
-                          weights='imagenet')
+    base_model = Xception(
+        input_shape=(299, 299, 3), include_top=False, weights="imagenet"
+    )
 
     # First train only the top layers
     for layer in base_model.layers:
@@ -21,15 +28,24 @@ def train_chesspiece_model():
     model = build_model(base_model)
 
     train_generator, validation_generator = data_generators(
-        preprocess_input, (299, 299), 64)
+        preprocess_input, (299, 299), 64
+    )
 
     callbacks = model_callbacks(5, "./models/Xception_pre.h5", 0.1, 10)
 
-    history = train_model(model, 20, train_generator, validation_generator,
-                          callbacks, use_weights=False, workers=5)
+    history = train_model(
+        model,
+        20,
+        train_generator,
+        validation_generator,
+        callbacks,
+        use_weights=False,
+        workers=5,
+    )
 
-    plot_model_history(history, "./models/Xception_pre_acc.png",
-                       "./models/Xception_pre_loss.png")
+    plot_model_history(
+        history, "./models/Xception_pre_acc.png", "./models/Xception_pre_loss.png"
+    )
     evaluate_model(model, validation_generator)
 
     # Also train blocks 12-14
@@ -38,16 +54,25 @@ def train_chesspiece_model():
     for layer in model.layers[106:]:
         layer.trainable = True
 
-    model.compile(optimizer='Adam', loss='categorical_crossentropy',
-                  metrics=['accuracy'])
+    model.compile(
+        optimizer="Adam", loss="categorical_crossentropy", metrics=["accuracy"]
+    )
 
     callbacks = model_callbacks(20, "./models/Xception.h5", 0.2, 8)
 
-    history = train_model(model, 100, train_generator, validation_generator,
-                          callbacks, use_weights=False, workers=5)
+    history = train_model(
+        model,
+        100,
+        train_generator,
+        validation_generator,
+        callbacks,
+        use_weights=False,
+        workers=5,
+    )
 
-    plot_model_history(history, "./models/Xception_acc.png",
-                       "./models/Xception_loss.png")
+    plot_model_history(
+        history, "./models/Xception_acc.png", "./models/Xception_loss.png"
+    )
     evaluate_model(model, validation_generator)
 
     model.save("./models/Xception_last.h5")
@@ -58,22 +83,32 @@ def continue_training():
     model = load_model("./models/Xception.h5")
 
     train_generator, validation_generator = data_generators(
-        preprocess_input, (299, 299), 64)
+        preprocess_input, (299, 299), 64
+    )
 
     # Train all layers
     for layer in model.layers:
         layer.trainable = True
 
-    model.compile(optimizer='Adam', loss='categorical_crossentropy',
-                  metrics=['accuracy'])
+    model.compile(
+        optimizer="Adam", loss="categorical_crossentropy", metrics=["accuracy"]
+    )
 
     callbacks = model_callbacks(20, "./models/Xception_all.h5", 0.2, 8)
 
-    history = train_model(model, 100, train_generator, validation_generator,
-                          callbacks, use_weights=False, workers=5)
+    history = train_model(
+        model,
+        100,
+        train_generator,
+        validation_generator,
+        callbacks,
+        use_weights=False,
+        workers=5,
+    )
 
-    plot_model_history(history, "./models/Xception_all_acc.png",
-                       "./models/Xception_all_loss.png")
+    plot_model_history(
+        history, "./models/Xception_all_acc.png", "./models/Xception_all_loss.png"
+    )
     evaluate_model(model, validation_generator)
 
     model.save("./models/Xception_all_last.h5")
